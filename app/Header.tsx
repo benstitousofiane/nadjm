4b6c5d098df4fdf2b2d883abe1cdf3b0507f0052
Benstitou Sofiane <129552238+benstitousofiane@users.noreply.github.com>
2025-01-05 23:47:46 +0100

    Update Header.tsx

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation"
 export default function Header(){
     return (
         <div className="flex border-2 border-white rounded text-white justify-center p-3 m-3 gap-1" onClick={() => redirect("/")}>
-            <p>Quran and tools for learn it</p>
+            <p>Quran and tools to learn it</p>
             <p className="text-yellow-200 glow">★</p>
             <p>نجم</p>
         </div>
